0244c69258069a2148d537a8b399164a2334a26c
muzammal kazmi <muzammalkazmi@gmail.com>
2020-12-25 13:04:38 +0500

    imported components in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 // import './App.css';
+import {Header} from './Header'
+import {Home} from './Home'
+import {Login} from './Login'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Home/>
+      <Login/>
     </div>
   );
 }
